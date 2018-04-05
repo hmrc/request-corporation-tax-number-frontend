4520a59e7e96cd8fc44841bdce1abf7442f27db9
Gareth Gooch <gareth.gooch@digital.hmrc.gov.uk>
2018-04-05 15:18:09 +0100

    added missing tests

---
+++ test/services/DmsSubmissionServiceSpec.scala
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package services
+
+import base.SpecBase
+import connectors.{CtutrConnector}
+import models._
+import org.mockito.Mockito._
+import org.mockito.{ArgumentCaptor, Matchers}
+import org.mockito.Matchers._
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.mockito.MockitoSugar
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.play.audit.http.connector.AuditResult
+import uk.gov.hmrc.play.bootstrap.audit.DefaultAuditConnector
+import utils.{MockUserAnswers}
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class DmsSubmissionServiceSpec extends SpecBase with MockitoSugar with ScalaFutures {
+
+  implicit val hcCaptor = ArgumentCaptor.forClass(classOf[HeaderCarrier])
+  implicit val ecCaptor = ArgumentCaptor.forClass(classOf[ExecutionContext])
+
+  ".ctutrSubmission" when {
+
+    "the submission is successful" must {
+
+      "return success" in {
+        val mockAuditConnector = mock[DefaultAuditConnector]
+
+        val answers = MockUserAnswers.minimalValidUserAnswers
+
+        val mockCtutrConnector = mock[CtutrConnector]
+        when(mockCtutrConnector.ctutrSubmission(any())(any(), any())) thenReturn Future.successful(Some(SubmissionResponse("id", "filename")))
+
+        val service = new DmsSubmissionService(frontendAppConfig, mockCtutrConnector, mockAuditConnector)
+        implicit val hc = new HeaderCarrier
+
+        val eventCaptor = ArgumentCaptor.forClass(classOf[SubmissionEvent])
+        val futureResult = service.ctutrSubmission(answers)
+
+        whenReady(futureResult) { result =>
+          result mustBe SubmissionSuccessful
+        }
+      }
+    }
+
+    "the submission fails" must {
+
+      "return failure" in {
+        val mockAuditConnector = mock[DefaultAuditConnector]
+
+        val answers = MockUserAnswers.minimalValidUserAnswers
+
+        val mockCtutrConnector = mock[CtutrConnector]
+        when(mockCtutrConnector.ctutrSubmission(any())(any(), any())) thenReturn Future.successful(None)
+
+        val service = new DmsSubmissionService(frontendAppConfig, mockCtutrConnector, mockAuditConnector)
+        implicit val hc = new HeaderCarrier
+
+        val eventCaptor = ArgumentCaptor.forClass(classOf[SubmissionEvent])
+        val futureResult = service.ctutrSubmission(answers)
+
+        whenReady(futureResult) { result =>
+          result mustBe SubmissionFailed
+        }
+      }
+    }
+  }
+}
