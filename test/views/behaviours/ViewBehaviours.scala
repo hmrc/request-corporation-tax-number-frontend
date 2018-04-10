008dbe7f27c85b10b0d79a0271d3da2edd1f1867
Gareth Gooch <gareth.gooch@digital.hmrc.gov.uk>
2018-04-10 11:07:37 +0100

    adding language toggle tests back in

--- test/views/behaviours/ViewBehaviours.scala
+++ test/views/behaviours/ViewBehaviours.scala
@@ -49,7 +49,7 @@ trait ViewBehaviours extends ViewSpecBase {
           for (key <- expectedGuidanceKeys) assertContainsText(doc, messages(s"$messageKeyPrefix.$key"))
         }
 
-        "display language toggles" ignore {
+        "display language toggles" in {
           val doc = asDocument(view())
           assertRenderedById(doc, "cymraeg-switch")
         }
