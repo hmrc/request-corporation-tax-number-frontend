0487de94551acd97c27f3ae85d6df0a04ba11da1
Andy Dowell <andydowell@administrators-MacBook-Pro.local>
2019-07-24 15:21:38 +0100

    DDCNOS-5513 - Lang controller upgraded to Play v4

---
+++ test/controllers/LanguageSwitchControllerSpec.scala
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import config.FrontendAppConfig
+import org.scalatestplus.play.OneAppPerSuite
+import play.api.Configuration
+import play.api.i18n.MessagesApi
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import uk.gov.hmrc.play.language.LanguageUtils
+import uk.gov.hmrc.play.test.UnitSpec
+
+class LanguageSwitchControllerSpec extends UnitSpec with OneAppPerSuite {
+
+  val messagesApi = app.injector.instanceOf(classOf[MessagesApi])
+  val langUtils : LanguageUtils = app.injector.instanceOf[LanguageUtils]
+  val config: Configuration = app.injector.instanceOf[Configuration]
+  val appConfig: FrontendAppConfig = app.injector.instanceOf[FrontendAppConfig]
+
+  object TestLanguageSwitchController extends LanguageSwitchController(config, appConfig, langUtils, messagesApi)
+
+  "Hitting language selection endpoint" must {
+
+    "redirect to English translated start page if English language is selected" in {
+      val request = FakeRequest()
+      val result = TestLanguageSwitchController.switchToLanguage("english")(request)
+      header("Set-Cookie", result) shouldBe Some("PLAY_LANG=en; Path=/;;PLAY_FLASH=switching-language=true; Path=/; HTTPOnly")
+    }
+
+    "redirect to Welsh translated start page if Welsh language is selected" in {
+      val request = FakeRequest()
+      val result = TestLanguageSwitchController.switchToLanguage("cymraeg")(request)
+      header("Set-Cookie", result) shouldBe Some("PLAY_LANG=cy; Path=/;;PLAY_FLASH=switching-language=true; Path=/; HTTPOnly")
+    }
+
+  }
+}
