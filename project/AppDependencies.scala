db5c78f636d3c1701356968532185cd61704db7f
daniel.manning <daniel.manning@digital.hmrc.gov.uk>
2019-12-24 11:23:18 +0000

    DDCNLS-6234: Add Dependencies file

---
+++ project/AppDependencies.scala
@@ -0,0 +1,33 @@
+import play.core.PlayVersion
+import play.sbt.PlayImport.ws
+import sbt.{ModuleID, _}
+
+object AppDependencies {
+
+  val compile: Seq[ModuleID] = Seq(
+    ws,
+    "uk.gov.hmrc" %% "play-reactivemongo" % "6.7.0",
+    "uk.gov.hmrc" %% "logback-json-logger" % "4.6.0",
+    "uk.gov.hmrc" %% "govuk-template" % "5.47.0-play-25",
+    "uk.gov.hmrc" %% "play-health" % "3.14.0-play-25",
+    "uk.gov.hmrc" %% "play-ui" % "8.5.0-play-25",
+    "uk.gov.hmrc" %% "http-caching-client" % "9.0.0-play-25",
+    "uk.gov.hmrc" %% "play-conditional-form-mapping" % "1.2.0-play-25",
+    "uk.gov.hmrc" %% "bootstrap-play-25" % "5.1.0",
+    "uk.gov.hmrc" %% "play-language" % "4.2.0-play-25",
+    "uk.gov.hmrc" %% "play-whitelist-filter" % "3.1.0-play-25"
+  )
+
+  val test: Seq[ModuleID] = Seq(
+    "uk.gov.hmrc" %% "hmrctest" % "3.9.0-play-25",
+    "org.scalatest" %% "scalatest" % "3.0.5",
+    "org.scalatestplus.play" %% "scalatestplus-play" % "2.0.1",
+    "org.pegdown" % "pegdown" % "1.6.0",
+    "org.jsoup" % "jsoup" % "1.11.3",
+    "com.typesafe.play" %% "play-test" % PlayVersion.current,
+    "org.mockito" % "mockito-all" % "1.10.19",
+    "org.scalacheck" %% "scalacheck" % "1.14.0"
+  ).map(_ % "test")
+
+  val all = compile ++ test
+}
