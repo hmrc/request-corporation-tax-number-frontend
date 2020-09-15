c552245da22bfabd2830f7fcfba8abdde5e118af
“meghna.sharma” <63240140+meghna2020@users.noreply.github.com>
2020-09-15 11:27:10 +0100

    DDCE-1057 Added code for Acessibility Statement

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -10,7 +10,7 @@ object AppDependencies {
     "uk.gov.hmrc" %% "logback-json-logger" % "4.8.0",
     "uk.gov.hmrc" %% "govuk-template" % "5.56.0-play-26",
     "uk.gov.hmrc" %% "play-health" % "3.15.0-play-26",
-    "uk.gov.hmrc" %% "play-ui" % "8.11.0-play-26",
+    "uk.gov.hmrc" %% "play-ui" % "8.12.0-play-26",
     "uk.gov.hmrc" %% "http-caching-client" % "9.1.0-play-26",
     "uk.gov.hmrc" %% "play-conditional-form-mapping" % "1.3.0-play-26",
     "uk.gov.hmrc" %% "bootstrap-play-26" % "1.14.0",
