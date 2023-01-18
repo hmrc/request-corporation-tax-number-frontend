461ab724b97873ee215be173635ac9d5ec251a53
stephenheath101 <98922374+stephenheath101@users.noreply.github.com>
2023-01-18 16:51:39 +0000

    Update AppDependencies.scala

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -12,7 +12,7 @@ object AppDependencies {
     "uk.gov.hmrc"       %% "play-conditional-form-mapping" % "1.11.0-play-28",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"    % "6.2.0",
     "uk.gov.hmrc"       %% "play-allowlist-filter"         % "1.1.0",
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"            % "3.21.0-play-28"
+    "uk.gov.hmrc"       %% "play-frontend-hmrc"            % "6.2.0-play-28"
   )
 
   val test: Seq[ModuleID] = Seq(
