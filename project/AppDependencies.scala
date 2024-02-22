40132dc4381e93df67fb57031a59b0f042bc6cd2
slavvco <13876870+slavvco@users.noreply.github.com>
2024-02-22 14:15:44 +0000

    DDCE-5376 | Updated dependencies, including play-frontend-hmrc (the Tudor Crown).

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,14 +9,14 @@ object AppDependencies {
     "uk.gov.hmrc"       %% "play-conditional-form-mapping" % "1.13.0-play-28",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"    % bootstrapVersion,
     "uk.gov.hmrc"       %% "play-allowlist-filter"         % "1.2.0",
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"            % "7.29.0-play-28"
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-28"    % "8.5.0"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "org.scalatest"          %% "scalatest"              % "3.2.17",
-    "org.scalatestplus"      %% "mockito-4-11"           % "3.2.17.0",
-    "org.scalatestplus"      %% "scalacheck-1-17"        % "3.2.17.0",
-    "org.jsoup"              %  "jsoup"                  % "1.16.2",
+    "org.scalatest"          %% "scalatest"              % "3.2.18",
+    "org.scalatestplus"      %% "mockito-5-10"           % "3.2.18.0",
+    "org.scalatestplus"      %% "scalacheck-1-17"        % "3.2.18.0",
+    "org.jsoup"              %  "jsoup"                  % "1.17.2",
     "org.scalacheck"         %% "scalacheck"             % "1.17.0",
     "com.vladsch.flexmark"   %  "flexmark-all"           % "0.64.8",
     "uk.gov.hmrc"            %% "bootstrap-test-play-28" % bootstrapVersion
