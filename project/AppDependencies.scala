23bffab7fa037b23b4ed900309d16301a823c0ee
alinakristy <alina.cristi12@gmail.com>
2026-03-25 09:39:55 +0000

    DDCE-8275 - Update deps

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,18 +2,18 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion: String = "10.5.0"
-  private val mongoVersion: String     = "2.11.0"
+  private val bootstrapVersion: String = "10.7.0"
+  private val mongoVersion: String     = "2.12.0"
 
   private val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"            % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                    % mongoVersion,
-    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.4.0",
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "12.25.0"
+    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.5.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "12.32.0"
   )
 
   private val test: Seq[ModuleID] = Seq(
-    "org.scalatestplus" %% "scalacheck-1-18"         % "3.2.19.0",
+    "org.scalatestplus" %% "scalacheck-1-19"         % "3.2.19.0",
     "uk.gov.hmrc"       %% "bootstrap-test-play-30"  % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30" % mongoVersion
   ).map(_ % "test")
